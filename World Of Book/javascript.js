6df743b0aa35c31a71f7e03a7dded310f6fad04e
Santiago766 <110736797+Santiago766@users.noreply.github.com>
2022-09-30 21:12:54 -0500

    Add files via upload

---
+++ World Of Book/javascript.js
@@ -0,0 +1,16 @@
+function BotonInicio() {
+    document.open(index.html)
+    document.write
+};
+
+function Categorias() {
+  var mensaje = "¿Que categoria deseas elegir?"
+
+  if (mensaje) {
+    alert(
+        "Has elegido accion"
+    );
+  } else {
+    alert("Has cerrado la ventana");
+  }
+};

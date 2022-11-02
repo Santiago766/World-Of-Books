738bd3ca20ab0f31cd9298f20ff11cf21cb8a791
Santiago766 <110736797+Santiago766@users.noreply.github.com>
2022-11-01 21:39:00 -0500

    Add files via upload
    arreglar menu

--- World Of Book2/javascript.js
+++ World Of Book2/javascript.js
@@ -1,1 +1,21 @@
+var popup = document.getElementById("Abrirpopumenu")
 
+StyleSheet.getElementById("po")
+
+function Abrirpopupmenu() {
+  popup.style.display = "block";
+}
+
+function CerrarPopupMenu() {
+  alert("eliecer puto");
+  }
+  
+
+
+  (() => {
+    let cerrar = getElementById('CerrarPopupMenu');
+
+    cerrar.addEventListener('click', () => {
+    })
+  }
+  )

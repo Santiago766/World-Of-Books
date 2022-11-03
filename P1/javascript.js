04fe133432f7ee634da561a4006afb670de9085e
Santiago766 <110736797+Santiago766@users.noreply.github.com>
2022-11-02 19:23:14 -0500

    Add files via upload

---
+++ P1/javascript.js
@@ -0,0 +1,8 @@
+
+function Abrirpopupmenu() {
+  document.getElementById("popup").style.display = "block"
+}
+function CerrarPopupMenu() {
+  document.getElementById("popup").style.display = "none"
+  }
+  
